113b4080151c1490e602aa2ac617615a9e616cc9
unknown <surajjamdade83086@gmail.com>
2024-10-24 19:07:51 +0530

    first commit

---
+++ assets/templates/basic/css/color.php
@@ -0,0 +1,19 @@
+
+
+:root{
+    --base: 253,
+            44,
+            121;
+            
+    --linear-bg: linear-gradient(50deg,#fd2c79, #ed0cbd);
+    --linear-bg-hover: linear-gradient(50deg,#ed0cbd, #fd2c79);
+            
+}
+
+.cookies-card a, .email-verify a{
+    color: #fd2c79;
+}
+.cookies-card a:hover{
+    color: #fd2c79;
+}
+
